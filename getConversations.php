315bb0fb6c75e81e17c3512fb3d8c9255671f0a8
Shaik Abdullah <abdullah2306@gmail.com>
2022-06-15 10:59:33 +0530

    just a backup

--- getConversations.php
+++ getConversations.php
@@ -32,7 +32,7 @@
     
   foreach($response as $res)
   {
-      $getUser ="SELECT id, name, picture, is_active, last_seen FROM users WHERE id = '$res' ";
+      $getUser ="SELECT id, name, picture, is_active, last_seen FROM users WHERE (`id` = '$res')";
 
       $userData = mysqli_query($con,$getUser);
 
