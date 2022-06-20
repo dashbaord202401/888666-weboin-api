ef2eb22c5236e407d5b7debb655c6a9f825c403a
x-4nto <72078695+x-4nto@users.noreply.github.com>
2022-06-20 10:42:48 +0530

    update database con

--- database.php
+++ database.php
@@ -2,24 +2,15 @@
 class Database{
 
     public $conn;
-
     public $assetsUrl = "https://app.weboin.com/api/users/assets/";
     public $adminAssetsUrl = "https://app.weboin.com/admin/images/";
-
-    // public $apiUsername = "weboin";
-    // public $apiPassword = "M^@^3JuqYpYJX3/q<?Fd'Fk}&z]~Q";
 
     // get the database connection
     public function getConnection(): ?mysqli
     {
-
-        // if($this->apiUsername == $authUsername && $this->apiPassword == $authPassword){
-
-            $this->conn = new mysqli("localhost:3306", "adminuser1", "6392RND8Cuxt", "weboin");
-            return $this->conn;
-
-        // }
-        return null;
+        $this->conn = new mysqli("localhost:3306", "adminuser1", "6392RND8Cuxt", "weboin");
+        // $this->conn = new mysqli("localhost", "root", "root", "web");
+        return $this->conn;
     }
 }
 ?>
